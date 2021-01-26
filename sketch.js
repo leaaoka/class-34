399a8be21a3543024f8de495ce184cd5479a6f07
Leaa <leaaoka@icloud.com>
2021-01-26 15:56:35 -0500

    database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,55 @@
 var ball;
 
+var database,ballPosRef
+var pos
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    ballPosRef = database.ref("ball/position")
+    ballPosRef.on("value",read, showErr)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    
+    database.ref("ball/position").set({
+        x: pos.x+x,
+        y: pos.y+y
+
+    })
+    
+    
+}
+
+function read(data){
+
+pos = data.val();
+ball.x = pos.x
+ball.y = pos.y
+
+
+}
+
+function showErr(){
+    console.log("Error")
 }
